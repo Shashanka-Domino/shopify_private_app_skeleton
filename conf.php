3fe6a0cb122ecd674ae27c5d2662ec8368f2f6fc
shashanka1 <shashankasrirama@gmail.com>
2016-12-13 10:37:57 -0600

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 
-	define('SHOPIFY_SHOP', 'your-store.myshopify.com');
-	define('SHOPIFY_APP_API_KEY', '');
-	define('SHOPIFY_APP_PASSWORD', '');
+	define('SHOPIFY_SHOP', 'producttagfilter.myshopify.com');
+	define('SHOPIFY_APP_API_KEY', '360edb7f98f0d59e0667ca1918a0c67f');
+	define('SHOPIFY_APP_PASSWORD', '4c224612c35f5127d03e83cd79ec308a');
 
 ?>
